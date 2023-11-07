b27449a080e8d04016e5278981c0586c989640c6
Ivan Vysotskyi <s26898@pjwstk.edu.pl>
2023-11-08 00:33:50 +0100

    changes in UI, the list screen works OK

--- app/src/main/java/com/example/shoppingtiger/MainActivity.kt
+++ app/src/main/java/com/example/shoppingtiger/MainActivity.kt
@@ -6,11 +6,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -18,9 +25,12 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -30,6 +40,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -37,6 +48,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.shoppingtiger.database.room.Item
 import com.example.shoppingtiger.ui.theme.ShoppingTigerTheme
 import kotlinx.coroutines.flow.collectLatest
 
@@ -62,19 +74,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ShoppingListItems(
-    viewModel:ShoppingListViewModel,
-    modifier: Modifier = Modifier) {
+    viewModel: ShoppingListViewModel,
+    modifier: Modifier = Modifier
+) {
 
     val listItems by viewModel.items.collectAsState(emptyList())
 
     LazyColumn(
-        modifier = Modifier.requiredHeight(500.dp)
-    ){
-        items(listItems) { item->
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top
+    ) {
+        items(listItems) { item ->
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(8.dp)
+                    .padding(4.dp)
                     .background(Color.White)
             ) {
                 // Checkbox on the right
@@ -86,18 +100,25 @@ fun ShoppingListItems(
                     }
                 )
 
-                // Shopping item name
-                Text(
-                    text = item.name,
+                //name
+                BasicTextField(
                     modifier = Modifier
-                        .weight(1f)
-                        .padding(8.dp),
-                    fontSize = 20.sp
+                        .padding(4.dp, 8.dp)
+                        .fillMaxWidth().weight(1f),
+                    value = item.name,
+                    onValueChange = { newName ->
+                        val itemCopy = item.copy(name = newName)
+                        viewModel.updatetItem(itemCopy)
+                    },
+                    singleLine = true,
+                    textStyle = TextStyle(fontSize = 20.sp)
                 )
 
                 //quantity
                 BasicTextField(
-                    modifier = Modifier.padding(8.dp),
+                    modifier = Modifier
+                        .padding(4.dp, 8.dp)
+                        .requiredWidth(20.dp),
                     value = item.quantity.toString(),
                     onValueChange = { updatedQuantity ->
                         val quantity = updatedQuantity.toIntOrNull() ?: 0
@@ -111,12 +132,49 @@ fun ShoppingListItems(
 
                 // Price
                 Text(
-                    text = "ZŁ${item.price}",
-                    modifier = Modifier.padding(8.dp),
+                    modifier = Modifier.padding(10.dp, 8.dp, 1.dp, 8.dp),
+                    text = "ZŁ",
                     fontSize = 18.sp
                 )
+                BasicTextField(
+                    modifier = Modifier
+                        .padding(4.dp, 8.dp)
+                        .requiredWidth(36.dp),
+                    value = item.price.toString(),
+                    onValueChange = { newPriceStr ->
+                        val newPrice = newPriceStr.toDoubleOrNull() ?: 0.0
+                        val itemCopy = item.copy(price = newPrice)
+                        viewModel.updatetItem(itemCopy)
+                    },
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                    singleLine = true,
+                    textStyle = TextStyle(fontSize = 18.sp)
+                )
+
+                //remove button
+                IconButton(
+                    onClick = {
+                        viewModel.deleteItem(item.id)
+                    }
+                ) {
+                    Icon(imageVector = Icons.Default.Delete, contentDescription = "Remove")
+                }
             }
             Divider()
+        }
+        item() {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = Color.White)
+                    .clickable {
+                        viewModel.insertItem(Item(name = "< new item >", quantity = 1))
+                    }
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text("+ Add New", fontSize = 20.sp, color = Color.Blue)
+            }
         }
     }
 }
