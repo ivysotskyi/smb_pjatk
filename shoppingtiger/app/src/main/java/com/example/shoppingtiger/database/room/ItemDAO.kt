d957b43289de9be2970361a2c67d82582f1de0db
Ivan Vysotskyi <s26898@pjwstk.edu.pl>
2023-12-06 00:18:36 +0100

    comment out DAO classes

--- shoppingtiger/app/src/main/java/com/example/shoppingtiger/database/room/ItemDAO.kt
+++ shoppingtiger/app/src/main/java/com/example/shoppingtiger/database/room/ItemDAO.kt
@@ -7,23 +7,23 @@ import androidx.room.Update
 import androidx.room.Delete
 import kotlinx.coroutines.flow.Flow
 
-@Dao
-interface ItemDAO {
-    @Query("SELECT * FROM items")
-    fun getItems(): Flow<List<Item>>
-
-    @Query("SELECT * FROM items WHERE item_id=:id")
-    fun getItem(id:Int): Flow<Item>
-
-    @Insert
-    suspend fun insertItem(item:Item) : Long
-
-    @Update
-    suspend fun updateItem(item:Item)
-
-    @Delete
-    suspend fun deleteItem(item:Item)
-
-    @Query("DELETE FROM ITEMS WHERE item_id = :id")
-    suspend fun deleteItem(id:Long)
-}
+//@Dao
+//interface ItemDAO {
+//    @Query("SELECT * FROM items")
+//    fun getItems(): Flow<List<Item>>
+//
+//    @Query("SELECT * FROM items WHERE item_id=:id")
+//    fun getItem(id:Int): Flow<Item>
+//
+//    @Insert
+//    suspend fun insertItem(item:Item) : Long
+//
+//    @Update
+//    suspend fun updateItem(item:Item)
+//
+//    @Delete
+//    suspend fun deleteItem(item:Item)
+//
+//    @Query("DELETE FROM ITEMS WHERE item_id = :id")
+//    suspend fun deleteItem(id:Long)
+//}
