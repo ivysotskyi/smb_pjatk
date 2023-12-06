6eafb0a892dfe4f46b6397c8392d21c23870a6f4
Ivan Vysotskyi <s26898@pjwstk.edu.pl>
2023-12-06 01:40:25 +0100

    implement per-user list

--- shoppingtiger/app/src/main/java/com/example/shoppingtiger/ui/theme/ItemsRepository.kt
+++ shoppingtiger/app/src/main/java/com/example/shoppingtiger/ui/theme/ItemsRepository.kt
@@ -7,83 +7,97 @@ import com.google.firebase.database.FirebaseDatabase
 import kotlinx.coroutines.flow.MutableStateFlow
 
 import androidx.compose.animation.core.snap
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseError
 
 class ItemsRepository(private val firebaseDatabase: FirebaseDatabase) {
 
     //val allItems = itemDao.getItems()
     val allItems = MutableStateFlow(HashMap<String, Item>())
+    private val userUid: String? = FirebaseAuth.getInstance().currentUser?.uid
+
+    private val eventsListener = object: ChildEventListener{
+
+        private fun getItemFromStnapshot(snapshot: DataSnapshot):Item {
+            return Item(
+                id = snapshot.ref.key as String,
+                name = (snapshot.child("name").value as? String)?:"",
+                price = (snapshot.child("price").value as? Double)?:(snapshot.child("price").value as? Long)?.toDouble()?:0.0,
+                purchased = (snapshot.child("purchased").value as? Boolean)?:false,
+                quantity = (snapshot.child("quantity").value as? Long)?:0
+            )
+        }
+
+        override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+            val item = getItemFromStnapshot(snapshot)
+            allItems.value = allItems.value.toMutableMap().apply {
+                put(item.id, item)
+            } as HashMap<String, Item>
+        }
+
+        override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+            val item = getItemFromStnapshot(snapshot)
+            allItems.value = allItems.value.toMutableMap().apply {
+                put(item.id, item)
+            } as HashMap<String, Item>
+        }
+
+        override fun onChildRemoved(snapshot: DataSnapshot) {
+            val item = getItemFromStnapshot(snapshot)
+            allItems.value = allItems.value.toMutableMap().apply {
+                remove(item.id)
+            } as HashMap<String, Item>
+        }
+
+        override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            TODO("Not yet implemented")
+        }
+
+    }
 
     init {
-        firebaseDatabase.getReference("items").addChildEventListener(object: ChildEventListener{
-
-            private fun getItemFromStnapshot(snapshot: DataSnapshot):Item {
-                return Item(
-                    id = snapshot.ref.key as String,
-                    name = (snapshot.child("name").value as? String)?:"",
-                    price = (snapshot.child("price").value as? Long)?.toDouble()?:0.0,
-                    purchased = (snapshot.child("purchased").value as? Boolean)?:false,
-                    quantity = (snapshot.child("quantity").value as? Long)?:0
-                )
-            }
-
-            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
-                val item = getItemFromStnapshot(snapshot)
-                allItems.value = allItems.value.toMutableMap().apply {
-                    put(item.id, item)
-                } as HashMap<String, Item>
-            }
-
-            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
-                val item = getItemFromStnapshot(snapshot)
-                allItems.value = allItems.value.toMutableMap().apply {
-                    put(item.id, item)
-                } as HashMap<String, Item>
-            }
-
-            override fun onChildRemoved(snapshot: DataSnapshot) {
-                val item = getItemFromStnapshot(snapshot)
-                allItems.value = allItems.value.toMutableMap().apply {
-                    remove(item.id)
-                } as HashMap<String, Item>
-            }
-
-            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
-            }
-
-        })
+        userUid?.let { uid ->
+            firebaseDatabase.getReference("private").child(uid).addChildEventListener(eventsListener)
+        }
     }
 
     fun insert(item: Item): String {
 
-        val inserted = firebaseDatabase.getReference("items").push()
+        userUid?.let { uid ->
+            val usersList = firebaseDatabase.getReference("private/${uid}")
 
-        inserted.child("name").setValue(item.name)
-        inserted.child("price").setValue(item.price)
-        inserted.child("purchased").setValue(item.purchased)
-        inserted.child("quantity").setValue(item.quantity)
+            val inserted = usersList.push()
 
-        return inserted.key ?: ""
+            inserted.child("name").setValue(item.name)
+            inserted.child("price").setValue(item.price)
+            inserted.child("purchased").setValue(item.purchased)
+            inserted.child("quantity").setValue(item.quantity)
+
+            return inserted.key ?: ""
+        }
+        return ""
     }
 
     fun update(item: Item) {
+        userUid?.let { uid ->
+            val itemToUpdate = firebaseDatabase.getReference("private/${uid}/${item.id}")
 
-        val itemToUpdate = firebaseDatabase.getReference("items/${item.id}")
-
-        itemToUpdate.child("name").setValue(item.name)
-        itemToUpdate.child("price").setValue(item.price)
-        itemToUpdate.child("purchased").setValue(item.purchased)
-        itemToUpdate.child("quantity").setValue(item.quantity)
+            itemToUpdate.child("name").setValue(item.name)
+            itemToUpdate.child("price").setValue(item.price)
+            itemToUpdate.child("purchased").setValue(item.purchased)
+            itemToUpdate.child("quantity").setValue(item.quantity)
+        }
     }
     fun delete(item: Item){
         delete(item.id)
     }
     fun delete(id: String){
-        firebaseDatabase.getReference("items/${id}").removeValue()
+        userUid?.let { uid ->
+            firebaseDatabase.getReference("private/${uid}/${id}").removeValue()
+        }
     }
 }
